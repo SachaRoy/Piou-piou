20ab668b5b5b1780372feb4a698dd248ffbbd7b1
sacharoy <sacha.roy.pro@gmail.com>
2024-08-13 10:46:03 +0200

    debut

---
+++ src/class.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+
+class player {
+private:
+    int score;
+    std::array<int, 4> hand;
+public:
+    int get_score();
+    void modif_hand();
+};
+
+
+class game {
+
+
+};
+
+class card {
+
+
+};
